500106dbb9df4256b2ff5d78644727ce69350520
srinivast-9 <52752688+srinivast-9@users.noreply.github.com>
2019-08-22 00:31:07 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,21 @@
+1. Method of running the program is as follows:
+
+C:\Users\Srinivas\Desktop\Set1Problem>python famtree.py C:\Users\Srinivas\Desktop\Set1Problem\Input.txt
+CHILD_ADDITION_SUCCEEDED
+Aria
+Jnki Ahit
+PERSON_NOT_FOUND
+PERSON_NOT_FOUND
+CHILD_ADDITION_FAILED
+NONE
+Satvy Krpi
+
+
+2. If no input test file is given as argument then following error occurs:
+C:\Users\Srinivas\Desktop\Set1Problem>python famtree.py
+No test file location given as argument.
+
+3. If wrong input file is given as argument then following error occurs:
+C:\Users\Srinivas\Desktop\Set1Problem>python famtree.py inp
+[Errno 2] No such file or directory: 'inp'
+
--- famtree.py
+++ famtree.py
@@ -1,3 +1,5 @@
+import sys
+
 _MALE_ = 'male'
 _FEMALE_ = 'female'
 
@@ -29,43 +31,38 @@ class Person:
             child.level = self.level + 1
             child.father = self.spouse
             child.mother = self.name
-            child.brothers = self.sons
-            child.sisters = self.daughters
             child.muncles = self.brothers
             child.maunts = self.sisters
+            child.puncles = self.blaws
+            child.paunts = self.slaws
+
+            for person in family:
+                for name in self.sons+self.daughters:
+                    if name == person.name:
+                        if person.gender == _MALE_:
+                            child.brothers.append(person.name)
+                            if person.spouse != '':
+                                child.slaws.append(person.spouse)
+                        else:
+                            child.sisters.append(person.name)
+                            if person.spouse != '':
+                                child.blaws.append(person.spouse)
+                        if child.gender == _MALE_:
+                            person.brothers.append(child.name)
+                        else:
+                            person.sisters.append(child.name)
+                        break
+                    elif name == person.spouse:
+                        if child.gender == _MALE_:
+                            person.blaws.append(child.name)
+                        else:
+                            person.slaws.append(child.name)
+                        break
 
             if child.gender == _MALE_:
                 self.sons.append(child.name)
             else:
                 self.daughters.append(child.name)
-            '''
-            for name in child.brothers:
-                for person in family:
-                    if name == person.name:
-                        child.slaws.append(person.spouse)
-                        break
-            for name in child.sisters:
-                for person in family:
-                    if name == person.name:
-                        child.blaws.append(person.spouse)
-                        break
-            for person in family:
-                if self.spouse == person.name:
-                    child.puncles = person.brothers
-                    child.paunts = person.sisters
-                    person.sons = self.sons
-                    person.daughters = self.daughters
-                    break
-            '''
-            for person in family:
-                for name in child.brothers:
-                    if name == person.name:
-                        child.slaws.append(person.spouse)
-                        break
-                for name in child.sisters:
-                    if name == person.name:
-                        child.blaws.append(person.spouse)
-                        break
 
             for person in family:
                 if self.spouse == person.name:
@@ -75,28 +72,108 @@ class Person:
                     person.daughters = self.daughters
                     break
 
-            #print('CHILD_ADDITION_SUCCEEDED')
             return True
         else:
-            #print('CHILD_ADDITION_FAILED')
             return False
 
     def add_spouse(self,spouse):
+        family = Person.by_name.values()
         if self.gender != spouse.gender and self.spouse=='' and spouse.spouse == '':
-            spouse.level = self.level
-            self.spouse = spouse.name
+            if spouse.level == 0 and self.level != 0:
+                spouse.level = self.level
+            elif spouse.level != 0 and self.level == 0:
+                self.level = spouse.level
+
             spouse.spouse = self.name
-            spouse.slaws += self.sisters
-            spouse.blaws += self.brothers
+            spouse.slaws = self.sisters
+            spouse.blaws = self.brothers
             spouse.sons = self.sons
             spouse.daughters = self.daughters
+            self.spouse = spouse.name
+            self.slaws = spouse.sisters
+            self.blaws = spouse.brothers
 
-            #print('SPOUSE_ADDITION_SUCCEEDED')
+            for person in family:
+                for name in self.brothers+self.sisters:
+                    if name == person.spouse:
+                        if person.gender == _MALE_:
+                            self.blaws.append(person.name)
+                            spouse.blaws.append(person.name)
+                            if person.spouse != '':
+                                spouse.slaws.append(person.spouse)
+                        else:
+                            self.slaws.append(person.name)
+                            spouse.slaws.append(person.name)
+                            if person.spouse != '':
+                                spouse.blaws.append(person.spouse)
+
+                        if self.gender == _MALE_:
+                            person.blaws.append(self.name)
+                            person.slaws.append(self.spouse)
+                        else:
+                            person.slaws.append(self.name)
+                            person.blaws.append(self.spouse)
+                        break
+
+                for name in spouse.brothers+spouse.sisters:
+                    if name == person.spouse:
+                        if person.gender == _MALE_:
+                            spouse.blaws.append(person.name)
+                            self.blaws.append(person.name)
+                            if person.spouse != '' :
+                                spouse.slaws.append(person.spouse)
+                        else:
+                            spouse.slaws.append(person.name)
+                            self.slaws.append(person.name)
+                            if person.spouse != '':
+                                spouse.blaws.append(person.spouse)
+
+                        if spouse.gender == _MALE_:
+                            person.blaws.append(spouse.name)
+                            person.slaws.append(spouse.spouse)
+                        else:
+                            person.slaws.append(spouse.name)
+                            person.blaws.append(spouse.spouse)
+                        break
+
+
+                for name in self.brothers+self.sisters:
+                    if name == person.name:
+                        if spouse.gender == _MALE_:
+                            person.blaws.append(spouse.name)
+                        else:
+                            person.slaws.append(spouse.name)
+
+                        if person.gender == _MALE_:
+                            spouse.blaws.append(person.name)
+                            if person.spouse != '':
+                                self.slaws.append(person.spouse)
+                        else:
+                            spouse.slaws.append(person.name)
+                            if person.spouse != '':
+                                self.blaws.append(person.spouse)
+                        break
+
+
+                for name in spouse.brothers + spouse.sisters:
+                    if name == person.name:
+                        if self.gender == _MALE_:
+                            person.blaws.append(self.name)
+                        else:
+                            person.slaws.append(self.name)
+
+                        if person.gender == _MALE_:
+                            self.blaws.append(person.name)
+                            if person.spouse != '' :
+                                spouse.slaws.append(person.spouse)
+                        else:
+                            self.slaws.append(person.name)
+                            if person.spouse != '':
+                                spouse.blaws.append(person.spouse)
+                        break
             return True
         else:
-            #print('SPOUSE_ADDITION_FAILED')
             return False
-
 
 def createFamilytree():
     shan = Person('King Shan',_MALE_,ishead=True)
@@ -169,14 +246,98 @@ def createFamilytree():
     krpi.add_child(kriya)
     krpi.add_child(krithi)
 
-    aria = Person('Aria',_FEMALE_)
-    if chitra.add_child(aria):
-        print('CHILD_ADDITION_SUCCEEDED')
-
-    print(lavnya.maunts)
-    print(aria.brothers+aria.sisters)
-    #for p in family:
-        #print(p.name)
 
 if __name__ == '__main__':
     createFamilytree()
+    family = Person.by_name.values()
+
+    try:
+        if len(sys.argv)<2:
+            print('No test file location given as argument.')
+            sys.exit(1)
+
+        with open(sys.argv[1],'r') as f:
+            for line in f.readlines():
+                if line != '' and line != '\n':
+                    command = line.strip('\n').split(' ')
+
+                if command[0].lower() == 'add_child' and len(command)==4:
+                    child = Person(command[2],command[3].lower())
+                    for person in family:
+                        if person.name == command[1]:
+                            if person.add_child(child):
+                                print('CHILD_ADDITION_SUCCEEDED')
+                            else:
+                                print('CHILD_ADDITION_FAILED')
+                            break
+                    else:
+                        print('PERSON_NOT_FOUND')
+
+                elif command[0].lower() == 'get_relationship' and len(command)==3:
+                    for person in family:
+                        if person.name == command[1]:
+                            relationship = command[2].lower()
+                            pstring = ''
+                            if relationship == 'paternal-uncle':
+                                if person.puncles:
+                                    for puncle in set(person.puncles):
+                                        pstring += puncle + ' '
+                                else:
+                                    pstring = 'NONE'
+                            elif relationship == 'maternal-uncle':
+                                if person.muncles:
+                                    for muncle in set(person.muncles):
+                                        pstring += muncle + ' '
+                                else:
+                                    pstring = 'NONE'
+                            elif relationship == 'paternal-aunt':
+                                if person.paunts:
+                                    for paunt in set(person.paunts):
+                                        pstring += paunt + ' '
+                                else:
+                                    pstring = 'NONE'
+                            elif relationship == 'maternal-aunt':
+                                if person.maunts:
+                                    for maunt in set(person.maunts):
+                                        pstring += maunt + ' '
+                                else:
+                                    pstring = 'NONE'
+                            elif relationship == 'sister-in-law':
+                                if person.slaws:
+                                    for slaw in set(person.slaws):
+                                        pstring += slaw + ' '
+                                else:
+                                    pstring = 'NONE'
+                            elif relationship == 'brother-in-law':
+                                if person.blaws:
+                                    for blaw in set(person.blaws):
+                                        pstring = blaw + ' '
+                                else:
+                                    pstring = 'NONE'
+                            elif relationship == 'son':
+                                if person.sons:
+                                    for son in set(person.sons):
+                                        pstring += son + ' '
+                                else:
+                                    pstring = 'NONE'
+                            elif relationship == 'daughter':
+                                if person.daughters:
+                                    for daughter in set(person.daughters):
+                                        pstring += daughter + ' '
+                                else:
+                                    pstring = 'NONE'
+                            elif relationship == 'siblings':
+                                siblings = person.brothers+person.sisters
+                                if siblings:
+                                    for sibling in set(siblings):
+                                        pstring += sibling + ' '
+                                else:
+                                    pstring = 'NONE'
+                            print(pstring)
+                            break
+                    else:
+                        print('PERSON_NOT_FOUND')
+                else:
+                    print('PERSON_NOT_FOUND')
+    except Exception as e:
+        print(e)
